404d84bfbe58c14825895c5f5658218218d6f325
tyleroverholts <tyoverholts@gmail.com>
2022-12-05 10:32:19 -0700

    added routes for get and post

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,46 @@
 const express = require('express');
-const knex = require('knex');
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    user: 'postgres',
+    password: 'docker',
+    database: 'miniapp_1'
+  }
+});
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use(express.json())
+app.use(bodyParser.json())
 
+app.get('/', async (req, res) => {
+  let movies
+  try {
+    movies = await knex('movies')
+      .select('*')
+      .then(rows => rows)
+    res.status(200).send(movies);
+  }catch(err) {
+    console.log(err);
+  }
+})
 
+app.post('/', async (req, res) => {
+  const { body } = req;
+  try {
+    let newMovie = await knex('movies')
+      .insert({title: `${body.title}`}, 'id')
+      .then(id => console.log(id))
+    let updatedMovies = await knex('movies')
+      .select('*')
+      .then(rows => rows)
+     res.status(200).send(updatedMovies)
+  }
+  catch(err){
+    console.log(err)
+  }
+})
 
 
 module.exports = app
