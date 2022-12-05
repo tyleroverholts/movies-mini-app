78ea61d6cd4bbe776d932f6ee88286050a8f0675
tyleroverholts <tyoverholts@gmail.com>
2022-12-05 08:28:37 -0700

    Level 0 completed

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+
+
+
+function App() {
+  const movies = [
+    {title: 'Mean Girls'},
+    {title: 'Hackers'},
+    {title: 'The Grey'},
+    {title: 'Sunshine'},
+    {title: 'Ex Machina'}
+  ]
+  return (
+    <div className="App">
+      {movies.map(movie =>
+        <p>{movie.title}</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
